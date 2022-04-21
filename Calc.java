4a28ce8c1ab7311943fe62699034778c904ec82b
saajraval <saajravals7r@gmail.com>
2022-04-21 07:05:06 +0530

    Addition of two nos

---
+++ Calc.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Calc{
+
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Enter two numbers :");
+		int n1 =sc.nextInt();
+		int n2= sc.nextInt();
+
+// printf() printf("\n ")
+// 3 + 4 = 7 
+// without using sum as 3rd 
+
+		int sum = n1+n2;
+		System.out.println("Sum : "+sum);
+		System.out.println(n1+" + " + n2 + " = " +sum);
+
+		System.out.println("Sum : "+(n1+n2));
+	}//end of main
+} // end of class calc
+
+//switch + - / * % 
---
+++ Test.java
@@ -0,0 +1,25 @@
+//import java.util.Scanner;
+// n - >class we make 
+
+
+public class Test{
+public static void main(String [] args){
+		System.out.println(" This is class Test ");
+	}
+}
+
+class A{
+	public static void main(String [] args){
+		System.out.println(" This is class A");
+	}
+}
+
+class B{
+	public static void main(String [] args){
+		System.out.println(" This is class B");
+	}
+}
+
+class C{
+
+}
